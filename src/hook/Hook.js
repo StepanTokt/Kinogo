a31bedc0e3acf223464defd951cd889d5a070bdf
StepanTokt <stepantokt@mail.ru>
2024-04-29 13:09:38 +0300

    choose right key API

--- src/hook/Hook.js
+++ src/hook/Hook.js
@@ -21,12 +21,12 @@ export const useHttp = () => {
                         headers: {
                             "Content-Type": 'application/json',
                             //  "X-API-KEY": '9KWQ7WJ-Q0E41WA-P1SBPF7-MCFWNET' 
-                            //"X-API-KEY": 'J3398TC-FWE4PYC-PY1W405-06TQ1W1'
+                            "X-API-KEY": 'J3398TC-FWE4PYC-PY1W405-06TQ1W1'
 
 
                             //"X-API-KEY": 'HC8GGHE-2BYMKNH-JJ0B48W-T2AJY0H'
                             //"X-API-KEY": '8SZWD2J-QREMTZG-KDXNCA2-YP16791' //alex
-                            "X-API-KEY": '1NSAF29-XK3MWNN-QTD61HP-FR42JQ6'
+                            //"X-API-KEY": '1NSAF29-XK3MWNN-QTD61HP-FR42JQ6'
 
 
                            
